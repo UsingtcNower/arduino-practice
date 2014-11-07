b6e4d72986c9594647b888b232cf54f45bd21ab3
nower <nowerzt@gmail.com>
2014-11-07 23:55:53 +0800

    add temperature data

--- dataManager.py
+++ dataManager.py
@@ -4,16 +4,26 @@ import MySQLdb
 import time
 
 ser = serial.Serial('/dev/ttyACM0', 9600, timeout=2)
-db = MySQLdb.connect(host='localhost', user='user', passwd='passwd', db='monitor')
-regx = re.compile(r'Humidity\s*=\s*(\d+)')
+db = MySQLdb.connect(host='localhost', user='root', passwd='653800', db='monitor')
+regx = r'(Humidity|Temperature) = (\d+)'
 line = ser.readline()
 while line :
-	match = regx.search(line)
+	#print line
+	match = re.findall(regx, line)
 	if(match):
-		value = match.group(1)
+		#print match
+		time_id = time.time()
+		humidity = match[0][1]
 		cursor = db.cursor()
-		cmd = "INSERT INTO humidity(time_id,humidity_value) VALUES({0}, {1})".format(time.time(), value)
+		cmd = "INSERT INTO humidity(time_id,humidity_value) VALUES({0}, {1})".format(time_id, humidity)
+		#print cmd
 		cursor.execute(cmd)
+		db.commit()
+		temperature = match[1][1]
+		cursor = db.cursor()
+		cmd = "INSERT INTO temperature(time_id,temperature_value) VALUES({0}, {1})".format(time_id, temperature)
+		cursor.execute(cmd)
+		#print cmd
 		db.commit()
 		#print cmd
 	line = ser.readline()
